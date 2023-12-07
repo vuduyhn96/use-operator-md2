fe01aa6f7e22413972abdb33acd027a3498fae94
vuduyhn96 <vuanhduyhn1996@gmail.com>
2023-12-07 11:12:02 +0700

    ifelse

---
+++ ifelse.java
@@ -0,0 +1,10 @@
+public class ifelse {
+    public static void main(String[] args) {
+        int age = 21;
+        if (age < 18)
+            System.out.println("chưa đủ tuổi đi tù");
+        else
+            System.out.println("đủ tuổi đi tù");
+        System.out.println("chúc mừng đã đi tù");
+    }
+}
